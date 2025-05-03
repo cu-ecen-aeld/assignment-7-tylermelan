4b8c86daf7eab86f91de8bd5110a5535ffb429c7
Tyler Melan <tymelan@gmail.com>
2025-05-03 10:23:45 -0700

    add github username to heelo.c printk

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -1,19 +1,19 @@
-/*                                                     
- * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $ 
- */                                                    
+/*
+ * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, tylermelan\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, tylermelan\n");
 }
 
 module_init(hello_init);
